b557323f2a15505795adc495323da0f0508770af
Antonio Posabella <16580320+Bellaposa@users.noreply.github.com>
2022-02-15 12:30:01 +0100

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Antonio Posabella on 15/02/22.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
